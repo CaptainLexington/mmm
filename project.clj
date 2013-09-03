7115acf9a1766dcd31c8323d126a251ca4fa460e
[Chris] Dale <dalex152@umn.edu>
2013-09-02 20:49:39 -0500

    Different implementation

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject mmm "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [korma "0.3.0-RC5"]
+                 [org.postgresql/postgresql "9.2-1002-jdbc4"]
+                 [ring/ring-jetty-adapter "1.1.6"]
+                 [compojure "1.1.3"]
+                 [hiccup "1.0.2"]
+                 ])
