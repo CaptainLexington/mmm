6945434866825cac0778e069ed1c6a0f64c8c491
Dale <dale@chrisdale.name>
2015-03-24 21:12:15 -0500

    TONIGHT, not tomorrow

--- src/mmm/scheduled.clj
+++ src/mmm/scheduled.clj
@@ -11,17 +11,6 @@
 
 
 
-
-(defn daily-tweets [time]
-  (let [screenings (map identity (screenings/one-day time))]
-    (doseq [screening screenings]
-      (twitter/tweet (str
-                       "TOMORROW: "
-                       (:tweet-text screening)
-                       "http://www.midnightmoviesmpls.com/screenings/"
-                       (:_id screening))))))
-
-
 (defn init-twitter-schedule []
   (chime-at
     (periodic-seq (.. (time/now)
@@ -29,7 +18,7 @@
                       (withTime 9 0 0 0))
                   (-> 1 time/days))
     (fn [time]
-      (daily-tweets time))))
+      (twitter/daily-tweets time))))
 
 
 
