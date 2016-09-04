f300c5db1949904f9723726d12ea3f5ed01644aa
Dale <dale@chrisdale.name>
2016-09-04 15:11:38 -0500

    System environments?

--- src/mmm/config.clj
+++ src/mmm/config.clj
@@ -2,11 +2,11 @@
 
 (def configs (read-string (slurp "config")))
 
-(def twitter-app-consumer-key (:twitter-consumer-key configs))
-(def twitter-app-consumer-secret (:twitter-consumer-secret configs))
-(def twitter-user-access-token (:twitter-user-access-token configs))
-(def twitter-user-access-token-secret (:twitter-user-access-secret configs))
+(def twitter-app-consumer-key (System/getenv "TWITTER_CONSUMER_KEY"))
+(def twitter-app-consumer-secret (System/getenv "TWITTER-CONSUMER-SECRET"))
+(def twitter-user-access-token (System/getenv "TWITTER_USER_ACCESS_TOKEN"))
+(def twitter-user-access-token-secret (System/getenv "TWITTER_USER_ACCESS_SECRET"))
 
 
-(def mongo-user (:mongo-user configs))
-(def mongo-db (:mongo-db configs))
+(def mongo-user (System/getenv "MONGO_USER"))
+(def mongo-db (System/getenv "MONGO_DB"))
